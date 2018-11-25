fad14acb2d3745b888e03530491d7a75ed324ace
jonesus <joonas.palosuo@gmail.com>
2018-11-25 02:21:45 +0200

    Finish rebranding

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import potato from '../assets/potato.webp';
 export const HomePage = () => (
   <>
     <h1 className="gradient-spin">Datato.me</h1>
+    <code>ˈdeɪtə tuː miː</code>
 
     <Link to="/browse" className="fingerprint">
       <img className="potato" src={potato} alt="" width={300} />
